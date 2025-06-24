85925e7ed3fb63823bea3eb8550b88f5407c6681
Paulo Sergio Fraga Marcos <paulosergiofragamarcos@gmail.com>
2025-06-23 21:50:22 -0300

    Add Avatar and Badge components; update Home page to include new UI elements and integrate Radix UI Avatar. Update package.json and package-lock.json to include @radix-ui/react-avatar dependency.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,11 @@
 import Header from "@/components/header"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { Card, CardContent } from "@/components/ui/card"
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
+import { Badge } from "@/components/ui/badge"
+import { Avatar, AvatarImage } from "@/components/ui/avatar"
 
 export default function Home() {
   return (
@@ -26,6 +29,21 @@ export default function Home() {
             className="rounded-xl object-cover"
           />
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex">
+            <div className="flex flex-col gap-2 py-5">
+              <Badge>Confirmado</Badge>
+              <h3 className="text-lg font-bold">Corte de cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/2f9278ba-3975-4026-af46-64af78864494-16u.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </>
   )
