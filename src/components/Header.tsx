09eea98ddb5fd9e356c080fa4872bbed1051fa96
Paulo Sergio Fraga Marcos <paulosergiofragamarcos@gmail.com>
2025-06-23 21:20:10 -0300

    Refactor Header component to include a Button with MenuIcon; update Card styling for better layout. Adjust CSS variables in globals.css for improved color scheme and remove unnecessary dark mode media query.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,17 @@
 import React from "react"
 import Image from "next/image"
 import { Card, CardContent } from "@/components/ui/card"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
 
 function Header() {
   return (
-    <Card>
-      <CardContent>
+    <Card className="rounded-t-none">
+      <CardContent className="flex flex-row items-center justify-between">
         <Image src="/logo.png" alt="logo" width={100} height={100} />
-        <h1>Header</h1>
+        <Button size="icon" variant="ghost">
+          <MenuIcon />
+        </Button>
       </CardContent>
     </Card>
   )
